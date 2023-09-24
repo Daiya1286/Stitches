9e7579798843f770efbfd823dd78acbe955e46ab
DaiyaCodes <dakshgotsandalphon@gmail.com>
2023-09-24 12:21:43 +0530

    Initial commit

---
+++ src/main/java/com/daiya/stitches/StitchesClient.java
@@ -0,0 +1,10 @@
+package com.daiya.stitches;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StitchesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
